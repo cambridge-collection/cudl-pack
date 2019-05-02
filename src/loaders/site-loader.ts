e992e91d37eefb730cfb48e0b82af34bc69a640e
Hal Blackburn <hwtb2@cam.ac.uk>
2019-05-02 14:58:50 +0100

    Remove dependency resolution from site-loader
    This is handled by json-dependencies-loader now.

--- src/loaders/site-loader.ts
+++ src/loaders/site-loader.ts
@@ -1,25 +1,11 @@
-import {urlToRequest} from 'loader-utils';
-import {promisify} from 'util';
 import webpack from 'webpack';
 import {parseSiteJson} from '../site';
-import {bindPromiseToCallback} from '../utils';
 
-const loader: webpack.loader.Loader = function(source: string) {
-    bindPromiseToCallback(load.call(this, source), this.async());
-}
+const loader: webpack.loader.Loader = (source: string) => {
+    // This just serves to ensure the input is valid site JSON
+    parseSiteJson(source);
 
-async function load(this: webpack.loader.LoaderContext, source: string): Promise<string> {
-    const site = parseSiteJson(source);
-
-    await Promise.all(site.collections.map((collectionRef) => {
-        const req = urlToRequest(collectionRef.href);
-        return promisify(this.resolve)(this.context, req)
-            .then((path) => {
-                this.addDependency(path);
-            });
-    }));
-
-    return JSON.stringify(site);
-}
+    return source;
+};
 
 export default loader;
