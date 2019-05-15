39c386aee7c4af3956f0c2150716e686cb1e7562
Hal Blackburn <hwtb2@cam.ac.uk>
2019-05-15 11:02:01 +0100

    Implement JSON5 -> JSON loader

---
+++ src/loaders/json-json5-loader.ts
@@ -0,0 +1,9 @@
+import JSON5 from 'json5';
+
+/**
+ * A loader which transforms JSON5 input into a JSON string.
+ *
+ * Like the `json5-loader`, except it produces a JSON module instead of a
+ * javascript module exporting a parsed JSON value.
+ */
+export default (source: string) => JSON.stringify(JSON5.parse(source));
---
+++ test/json-json5-loader.test.ts
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import path from 'path';
+import {promisify} from 'util';
+import webpack from 'webpack';
+
+import {default as loader} from '../src/loaders/json-json5-loader';
+import compiler from './compiler';
+
+const decodedExampleJson5 = {
+    unquoted: 'and you can quote me on that',
+    singleQuotes: 'I can use "double quotes" here',
+    lineBreaks: "Look, Mom! No \\n's!",
+    hexadecimal: 0xdecaf,
+    leadingDecimalPoint: .8675309, andTrailing: 8675309.,
+    positiveSign: +1,
+    trailingComma: 'in objects', andIn: ['arrays'],
+    backwardsCompatible: 'with JSON',
+};
+
+test('loader function converts json5 to json', async () => {
+    const json5 = await promisify(fs.readFile)(
+        path.resolve(__dirname, 'data/json5/example.json5'), {encoding: 'utf-8'});
+
+    const result = loader(json5);
+    expect(typeof result).toBe('string');
+    expect(JSON.parse(result)).toEqual(decodedExampleJson5);
+});
+
+test('webpack loads json5 to json with loader ', async () => {
+    const rules: webpack.RuleSetRule[] = [{
+        type: 'json',
+        test: /\.json5$/,
+        use: path.resolve(__dirname, '../src/loaders/json-json5-loader.ts'),
+    }];
+
+    const stats = await compiler('./data/json5/example.json5', rules);
+    const module = stats.toJson().modules[0];
+
+    expect(stats.compilation.modules[0].type).toEqual('json');
+    expect(JSON.parse(module.source)).toEqual(decodedExampleJson5);
+});
+
+test('webpack reports error when loading invalid json5', async () => {
+    const rules: webpack.RuleSetRule[] = [{
+        type: 'json',
+        test: /\.json5$/,
+        use: path.resolve(__dirname, '../src/loaders/json-json5-loader.ts'),
+    }];
+
+    try {
+        await compiler('./data/json5/invalid.json5', rules);
+        fail();
+    }
+    catch(error) {
+        expect(error).toMatch(/\.\/data\/json5\/invalid\.json5/);
+        expect(error).toMatch(/Module build failed \(from \.\.\/src\/loaders\/json-json5-loader\.ts\)/);
+        expect(error).toMatch(/SyntaxError: JSON5: /);
+    }
+});
