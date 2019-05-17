0d6cb91fd7be8c82bcfc4b342bdabd3fd506d6bc
Hal Blackburn <hwtb2@cam.ac.uk>
2019-05-17 17:50:02 +0100

    Fix lint issues

--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,5 @@ export function bindPromiseToCallback<T>(
     promise: Promise<T>, callback: (err: any, value?: T) => void): void {
 
     promise.then(
-        (value) => {callback(null, value);}, callback);
+        (value) => { callback(null, value); }, callback);
 }
