40c77fb04043e34b538c75efbda9f7a429319d33
Hal Blackburn <hwtb2@cam.ac.uk>
2019-06-27 13:03:28 +0100

    Handle inline query options in json-format-loader

--- src/loaders/json-format-loader.ts
+++ src/loaders/json-format-loader.ts
@@ -6,7 +6,7 @@ import webpack from 'webpack';
 import {createValidator} from '../schemas';
 import optionsSchema from './json-format-loader-options.schema.json';
 
-interface Options {
+export interface Options {
     /** An int 0-10 or string with 0-10 tabs and or spaces. */
     indent?: number | string;
     /** Whether to insert a newline after the end of the JSON doc. Default: yes if the doc is indented. */
@@ -23,23 +23,34 @@ const validateOptions = createValidator<Options>({
  * A loader which re-indents JSON.
  */
 export default function(this: webpack.loader.LoaderContext, source: string): string {
-    const options: Options = validateOptions(loaderUtils.getOptions(this || {}));
-    if(options.indent !== undefined)
-        validateIndent(options.indent);
-    const finalNewline = options.finalNewline === undefined ? !!options.indent : options.finalNewline;
-
-    return JSON.stringify(parseJson(source), undefined, options.indent) + (finalNewline ? '\n' : '');
+    const options: Options = normaliseOptions(validateOptions(loaderUtils.getOptions(this) || {}));
+    return JSON.stringify(parseJson(source), undefined, options.indent) + (options.finalNewline ? '\n' : '');
 }
 
-function validateIndent(indent: number | string) {
-    if(typeof indent === 'string' && /^[ \t]{0,10}$/.test(indent))
-            return;
-    else if(typeof indent === 'number') {
-        if(Number.isInteger(indent) && indent >= 0 && indent < 11)
-            return;
+function normaliseOptions(options: Options): Options {
+    let indent = options.indent;
+    let normalisedIndent;
+    if(typeof indent === 'string') {
+        if(/^[ \t]{0,10}$/.test(indent))
+            normalisedIndent = indent;
+        else if(/^[\d+]$/.test(indent))
+            indent = parseInt(indent, 10);
     }
+    if(typeof indent === 'number') {
+        if(Number.isInteger(indent) && indent >= 0 && indent < 11)
+            normalisedIndent = indent;
+    }
+    if(indent === undefined)
+        normalisedIndent = 0;
 
-    throw new Error(`\
+    if(normalisedIndent === undefined) {
+        throw new Error(`\
 indent option must be an integer >= 0 and <= 10, or an equivalent length string containing only tabs and or spaces; \
 got: ${util.inspect(indent)}`);
+    }
+
+    return {
+        indent: normalisedIndent,
+        finalNewline: options.finalNewline === undefined ? !!normalisedIndent : options.finalNewline,
+    };
 }
--- test/json-format-loader.test.ts
+++ test/json-format-loader.test.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import {Options} from '../src/loaders/json-format-loader';
 import compiler from './compiler';
 
 const noIndent = '{"thisIs":"c"}';
@@ -12,21 +13,29 @@ const indentTabSpace = `\
 \t "thisIs": "c"
 }\n`;
 
-test.each<[number | string | undefined, string]>([
-    [undefined, noIndent],
-    [0, noIndent],
-    ['', noIndent],
-    [2, indent2],
-    ['  ', indent2],
-    ['\t ', indentTabSpace],
-])('loader formats JSON with %j', async (indent, expectedJson) => {
-    const options = indent === undefined ? {} : {indent};
-
+test.each<[string, Options | undefined, string]>([
+    ['', undefined, noIndent],
+    ['', {indent: 0}, noIndent],
+    ['', {finalNewline: true}, noIndent + '\n'],
+    ['', {indent: ''}, noIndent],
+    ['', {indent: 2}, indent2],
+    ['', {indent: 2, finalNewline: false}, indent2.trimRight()],
+    ['', {indent: '  '}, indent2],
+    ['', {indent: '\t '}, indentTabSpace],
+    ['?', undefined, noIndent],
+    ['?finalNewline=true', undefined, noIndent + '\n'],
+    ['?indent=', undefined, noIndent],
+    ['?indent=0', undefined, noIndent],
+    ['?indent=2', undefined, indent2],
+    ['?indent=2&finalNewline=false', undefined, indent2.trimRight()],
+    ['?indent=%20%20', undefined, indent2],
+    ['?indent=%09%20', undefined, indentTabSpace],
+])('loader formats JSON with query %j and options %j', async (query, options, expectedJson) => {
     const rules: webpack.RuleSetRule[] = [{
         type: 'json',
         test: /\.json$/,
         use: {
-            loader: path.resolve(__dirname, '../src/loaders/json-format-loader.ts'),
+            loader: path.resolve(__dirname, '../src/loaders/json-format-loader.ts') + query,
             options,
         },
     }];
