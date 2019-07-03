dac7898d128bbf22feae2616b7315cdd73d14b93
Hal Blackburn <hwtb2@cam.ac.uk>
2019-07-03 12:07:04 +0100

    Implement loading item links differently based on role
    The ItemDataLinkDependencyPlugin — used with json-dependencies-loader —
    can create module requests for linked resources, with varying request
    query values, allowing dependencies to be loaded multiple times with
    different loaders. E.g. one loader might extract a resource to the
    output, while another loads its data to merge with the item (think
    linked TEI data which is both the basis for an item, and needs to
    be available downstream).

---
+++ src/loaders/item-data-link-dependency-plugin.ts
@@ -0,0 +1,51 @@
+import {urlToRequest} from 'loader-utils';
+import webpack from 'webpack';
+import {getData, NamespaceLoader} from '../item';
+import {isLinkItemData, LinkItemData} from '../item-types';
+import {validateItem} from '../schemas';
+import {DependencyResolutionHooks, PluginObject, Reference} from './json-dependencies-loader';
+
+type LoaderContext = webpack.loader.LoaderContext;
+
+interface Options {
+    /** The roles that the link data items must have to be considered. */
+    roles?: string | Iterable<string>;
+
+    /** A query string to add to requests generated from discovered links. */
+    requestQuery?: string;
+}
+
+/**
+ * A plugin for the json-dependencies-loader which can create dependencies for
+ * links with a specific set of roles with a specified query param.
+ */
+export class ItemDataLinkDependencyPlugin implements PluginObject {
+    public static TAP_NAME = 'ItemDataLinkDependencyPlugin';
+
+    private readonly roles: Set<string>;
+    private readonly requestQuery: string | null;
+
+    constructor({roles, requestQuery}: Options) {
+        this.roles = new Set(typeof roles === 'string' ? [roles] : roles || []);
+        this.requestQuery = requestQuery || null;
+    }
+
+    public apply(hooks: DependencyResolutionHooks) {
+        const options = ItemDataLinkDependencyPlugin.TAP_NAME;
+        hooks.findReferences.tapPromise(options, async (references: Reference[], doc: any, context: LoaderContext) => {
+            const item = validateItem(doc);
+            const ns = await NamespaceLoader.forWebpackLoader(context).loadNamespace(item);
+            const links: LinkItemData[] = getData(item, ns, {type: isLinkItemData, roles: this.roles});
+
+            links.forEach(data => {
+                let request = urlToRequest(data.href['@id']);
+                if(this.requestQuery) {
+                    request = `${request}?${this.requestQuery}`;
+                }
+                references.push({request});
+            });
+
+            return references;
+        });
+    }
+}
---
+++ test/item-data-link-dependency-plugin.test.ts
@@ -0,0 +1,42 @@
+import * as path from 'path';
+import webpack from 'webpack';
+import {ItemDataLinkDependencyPlugin} from '../src/loaders/item-data-link-dependency-plugin';
+import {CDLRole} from '../src/uris';
+import compiler from './compiler';
+import {readPathAsString} from './util';
+
+test('item data links are handled specially by ItemDataLinkDependencyPlugin', async () => {
+    // Here we load the data.json dependency linked from item.json in two ways based on @role values.
+
+    const dir = './data/item-data-link-dependency-plugin';
+    const item = `${dir}/item.json`;
+    const rules: webpack.RuleSetRule[] = [
+        {
+            test: path.resolve(__dirname, item),
+            use: [
+                {loader: '../src/loaders/json-dependencies-loader.ts',
+                 options: {plugins: [new ItemDataLinkDependencyPlugin({roles: [CDLRole.curie.uri('foo')],
+                                                                        requestQuery: 'a'})]}},
+                {loader: '../src/loaders/json-dependencies-loader.ts',
+                 options: {plugins: [new ItemDataLinkDependencyPlugin({roles: [CDLRole.curie.uri('baz'),
+                                                                               CDLRole.curie.uri('bar')],
+                                                                       requestQuery: 'b'})]}},
+            ],
+        },
+        {test: /\/data\.json$/, resourceQuery: '?a',
+         use: '../src/loaders/json-wrap-loader.ts?{insertionPoint: "/data", template: {handledBy: "a"}}'},
+        {test: /\/data\.json$/, resourceQuery: '?b',
+            use: '../src/loaders/json-wrap-loader.ts?{insertionPoint: "/data", template: {handledBy: "b"}}'},
+    ];
+
+    const stats = (await compiler(item, rules)).toJson();
+    const [mainModule, dataA, dataB] = [
+        stats.modules.filter((m: any) => m.name === item)[0],
+        stats.modules.filter((m: any) => m.name === `${dir}/data.json?a`)[0],
+        stats.modules.filter((m: any) => m.name === `${dir}/data.json?b`)[0],
+    ];
+
+    expect(JSON.parse(mainModule.source)).toEqual(JSON.parse(await readPathAsString(item)));
+    expect(JSON.parse(dataA.source)).toEqual({handledBy: 'a', data: {data: 42}});
+    expect(JSON.parse(dataB.source)).toEqual({handledBy: 'b', data: {data: 42}});
+});
