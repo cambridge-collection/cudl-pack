756119cd8eb4df903450f1903c1d9f1ebd842762
jlf44 <jlf44@cam.ac.uk>
2019-07-17 16:06:04 +0100

    add a loader to generate the pages element from a linked CSV resource with a mapping between image filename and foliation

---
+++ src/loaders/json-item-image-page-mapping-loader.ts
@@ -0,0 +1,106 @@
+import fs from 'fs';
+import loaderUtils from 'loader-utils';
+import path from 'path';
+import util from 'util';
+import webpack from 'webpack';
+import {createAsyncLoader} from '../utils';
+
+/**
+ * A loader which uses the CSV image mapping defined in the additional metadata
+ * section of the item JSON to generate a image-page foliation mapping and
+ * insert that into the item JSON.  Generating a new item JSON with the foliation
+ * and without the external mapping.
+ *
+ * Note: if there is already data in the pages array the contents of the linked
+ * CSV resource will override this data.
+ *
+ * TODO: Deal with transcriptions and translations that are on a page level.
+ */
+
+interface CSVRow {
+    Filename: string;
+    Foliation: string;
+    [key: string]: string;
+}
+
+interface JSONPage {
+    label: string;
+    resources: object[];
+    order: string;
+    [key: string]: any;
+}
+
+interface Options {
+    imageServerPath: string;
+}
+
+async function parseCSV(csvPath: string): Promise<object[]> {
+    const csv = require('neat-csv');
+
+    const readFile = util.promisify(fs.readFile);
+    const csvFile = await readFile(csvPath);
+    return csv(csvFile, { separator: '|', headers: ['Filename', 'Foliation'] });
+
+}
+
+async function load(this: webpack.loader.LoaderContext, source: string): Promise<string> {
+
+    // Get the image server path
+    const options = loaderUtils.getOptions(this);
+    if (!options.imageServerPath) {
+        return Promise.reject('You need to set the imageServerPath parameter in your loader options.');
+    }
+
+    const imageServerPath = (options as Options).imageServerPath;
+
+    // Find the path to the CSV pagination in the JSON item data.
+    const json = JSON.parse(source);
+
+    let csvPath = null;
+
+    for (const dataItem of json.data) {
+        const type = dataItem['@type'];
+        const role = dataItem['@role'];
+        if (type === 'cdl-data:link' &&
+           role === 'cdl-role:pagination') {
+            csvPath = dataItem.href['@id'];
+        }
+    }
+
+    // If there is no pagination linked in the metadata return the source JSON unchanged.
+    if (!csvPath) {
+        return source;
+    }
+
+    // Validate and parse the CSV
+    csvPath = path.resolve(path.dirname(this.resource), csvPath);
+    const csv: object[] = await parseCSV(csvPath);
+
+    // Generate the item JSON pages section from the pagination information.
+    let order: number = 1;
+    const newPages: JSONPage[] = [];
+    for (const row of csv) {
+
+        const filename = (row as CSVRow).Filename;
+        const foliation = (row as CSVRow).Foliation;
+
+        const page: JSONPage = {
+                    label: foliation,
+                    resources:  JSON.parse('{ "@type": "cdl-page:image",' +
+                        '"imageType": "iiif",' +
+                        '"image": {"@id": "' + imageServerPath + filename + '"}' +
+                        '}'),
+                    order: order.toString(),
+        };
+        newPages.push(page);
+        order = order ++;
+    }
+
+    // Insert into the item package JSON and return.
+    json.pages = newPages;
+
+    return JSON.stringify(json);
+
+}
+
+export default createAsyncLoader(load);
---
+++ test/json-item-image-page-mapping-loader.test.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+import webpack from 'webpack';
+import compiler from './compiler';
+import {readPathAsString} from './util';
+
+const rules: webpack.RuleSetRule[] = [{
+    type: 'json',
+    test: /\.json$/,
+    use: {
+        loader: path.resolve(__dirname, '../src/loaders/json-item-image-page-mapping-loader.ts'),
+        options: {imageServerPath: 'https://images.example.ac.uk/iiif/'},
+    }}];
+
+test('test loading CSV file and generating page mapped item JSON', async () => {
+
+    const stats = await compiler('./data/item/image-mapping/item-with-linked-pagination.json', rules);
+    const module = stats.toJson().modules[0];
+
+    const data: string = await readPathAsString('./data/item/image-mapping/item-with-pagination-inserted.json');
+
+    expect(JSON.parse(module.source)).toEqual(JSON.parse(data));
+
+});
