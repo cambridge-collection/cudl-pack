33390dbf5c88218299e73a256c019e33ac9615d3
jlf44 <jlf44@cam.ac.uk>
2019-07-24 13:53:40 +0100

    implementing sugguestions from code review

--- test/json-item-image-page-mapping-loader.test.ts
+++ test/json-item-image-page-mapping-loader.test.ts
@@ -1,5 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
+import {generateItemJson, parseItemJson} from '../src/item';
+import {Item} from '../src/item-types';
 import compiler from './compiler';
 import {readPathAsString} from './util';
 
@@ -7,7 +9,7 @@ const rules: webpack.RuleSetRule[] = [{
     type: 'json',
     test: /\.json$/,
     use: {
-        loader: path.resolve(__dirname, '../src/loaders/json-item-image-page-mapping-loader.ts'),
+        loader: path.resolve(__dirname, '../src/loaders/item-image-page-mapping-loader.ts'),
         options: {
             imageServerPath: 'https://images.example.ac.uk/iiif/',
             imageType: 'iiif',
@@ -19,8 +21,58 @@ test('test loading CSV file and generating page mapped item JSON', async () => {
     const stats = await compiler('./data/item/image-mapping/item-with-linked-pagination.json', rules);
     const module = stats.toJson().modules[0];
 
-    const data: string = await readPathAsString('./data/item/image-mapping/item-with-pagination-inserted.json');
+    // validate the input against item schema
+    const dataInput: string = await readPathAsString('./data/item/image-mapping/item-with-linked-pagination.json');
+    const itemIn: Item = parseItemJson(generateItemJson(JSON.parse(dataInput)));
+    expect(itemIn).toEqual(JSON.parse(dataInput));
 
-    expect(JSON.parse(module.source)).toEqual(JSON.parse(data));
+    // validate the output against item schema
+    const dataOutput: string = await readPathAsString('./data/item/image-mapping/item-with-pagination-inserted.json');
+    const itemOut: Item = parseItemJson(generateItemJson(JSON.parse(dataOutput)));
+    expect(itemOut).toEqual(JSON.parse(dataOutput));
+
+    // validate JSON generated against expected value.
+    expect(JSON.parse(module.source)).toEqual(JSON.parse(dataOutput));
+
+});
+
+test('test missing option imageServerPath', async () => {
+
+    const invalidRules: webpack.RuleSetRule[] = [{
+        type: 'json',
+        test: /\.json$/,
+        use: {
+            loader: path.resolve(__dirname, '../src/loaders/item-image-page-mapping-loader.ts'),
+            options: {
+                imageType: 'iiif',
+            },
+        }}];
+
+    await expect(compiler('./data/item/image-mapping/item-with-linked-pagination.json', invalidRules)).rejects
+        .toMatch(/You need to set the imageServerPath and imageType parameters in your loader options./);
+
+});
+
+test('test missing option imageType', async () => {
+
+    const invalidRules: webpack.RuleSetRule[] = [{
+        type: 'json',
+        test: /\.json$/,
+        use: {
+            loader: path.resolve(__dirname, '../src/loaders/item-image-page-mapping-loader.ts'),
+            options: {
+                imageServerPath: 'https://images.example.ac.uk/iiif/',
+            },
+        }}];
+
+    await expect(compiler('./data/item/image-mapping/item-with-linked-pagination.json', invalidRules)).rejects
+        .toMatch(/You need to set the imageServerPath and imageType parameters in your loader options./);
+
+});
+
+test('test invalid CSV', async () => {
+
+    await expect(compiler('./data/item/image-mapping/invalid-item-with-linked-pagination.json', rules)).rejects
+        .toMatch(/Problem parsing CSV. Each row shouldcontain <url>|<label>/);
 
 });
