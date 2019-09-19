1cbb48f137e7682cf6f22f2b75665bb65f169b95
Hal Blackburn <hwtb2@cam.ac.uk>
2019-09-19 08:40:40 +0100

    Remove unnecessary schema-utils dependency

--- src/loaders/json-wrap-loader.ts
+++ src/loaders/json-wrap-loader.ts
@@ -1,10 +1,12 @@
+import Ajv from 'ajv';
 import clone from 'clone';
 import jsonpointer from 'jsonpointer';
 import {getOptions} from 'loader-utils';
-import validateOptions from 'schema-utils';
 import webpack from 'webpack';
+import {createValidator} from '../schemas';
 
 const optionsSchema = {
+    $id: 'cudl-pack/loaders/json-wrap-loader.schema.json',
     type: 'object',
     properties: {
         insertionPoint: { type: 'string' },
@@ -18,12 +20,17 @@ interface Options {
     template?: object;
 }
 
+const validateOptions = createValidator<Options>({
+    schemaId: optionsSchema.$id,
+    validate: new Ajv().compile(optionsSchema),
+    name: 'options',
+});
+
 /**
  * A loader which nests the loaded value into a JSON structure
  */
 export default function(this: webpack.loader.LoaderContext, source: string) {
-    const options = getOptions(this) || {} as Options;
-    validateOptions(optionsSchema, options, 'json-wrap-loader');
+    const options = validateOptions(getOptions(this) || {});
 
     const template = clone(options.template || {}, false);
     jsonpointer.set(template, options.insertionPoint, JSON.parse(source));
