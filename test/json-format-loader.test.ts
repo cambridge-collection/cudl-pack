1dde2e131821b770d57915347d1c45de817d73e1
Hal Blackburn <hwtb2@cam.ac.uk>
2021-10-19 09:38:35 +0100

    refactor: update json-format-loader tests for webpack 5

--- test/json-format-loader.test.ts
+++ test/json-format-loader.test.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import webpack from 'webpack';
 import {Options} from '../src/loaders/json-format-loader';
 import compiler from './compiler';
-import {ensureDefined} from './util';
+import {getModuleSource} from './util';
 
 const noIndent = '{"thisIs":"c"}';
 const indent2 = `\
@@ -14,34 +14,25 @@ const indentTabSpace = `\
 \t "thisIs": "c"
 }\n`;
 
-test.each<[string, Options | undefined, string]>([
-    ['', undefined, noIndent],
-    ['', {indent: 0}, noIndent],
-    ['', {finalNewline: true}, noIndent + '\n'],
-    ['', {indent: ''}, noIndent],
-    ['', {indent: 2}, indent2],
-    ['', {indent: 2, finalNewline: false}, indent2.trimRight()],
-    ['', {indent: '  '}, indent2],
-    ['', {indent: '\t '}, indentTabSpace],
-    ['?', undefined, noIndent],
-    ['?finalNewline=true', undefined, noIndent + '\n'],
-    ['?indent=', undefined, noIndent],
-    ['?indent=0', undefined, noIndent],
-    ['?indent=2', undefined, indent2],
-    ['?indent=2&finalNewline=false', undefined, indent2.trimRight()],
-    ['?indent=%20%20', undefined, indent2],
-    ['?indent=%09%20', undefined, indentTabSpace],
-])('loader formats JSON with query %j and options %j', async (query, options, expectedJson) => {
+test.each<[Options | undefined, string]>([
+    [undefined, noIndent],
+    [{indent: 0}, noIndent],
+    [{finalNewline: true}, noIndent + '\n'],
+    [{indent: ''}, noIndent],
+    [{indent: 2}, indent2],
+    [{indent: 2, finalNewline: false}, indent2.trimRight()],
+    [{indent: '  '}, indent2],
+    [{indent: '\t '}, indentTabSpace],
+])('loader formats JSON with options %j', async (options, expectedJson) => {
     const rules: webpack.RuleSetRule[] = [{
         type: 'json',
         test: /\.json$/,
         use: {
-            loader: path.resolve(__dirname, '../src/loaders/json-format-loader.ts') + query,
+            loader: path.resolve(__dirname, '../src/loaders/json-format-loader.ts'),
             options,
         },
     }];
 
     const stats = await compiler('./data/references/c.json', rules);
-    const module = ensureDefined.wrap(stats.toJson()).modules[0];
-    expect(module.source).toBe(expectedJson);
+    expect(getModuleSource('./data/references/c.json', stats)).toEqual(expectedJson);
 });
